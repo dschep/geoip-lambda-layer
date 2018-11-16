067295fdbb2a39355b859bf66f2a2bea2ec91bda
Daniel Schep <dschep@gmail.com>
2018-11-16 09:25:38 -0500

    example project with a lambda using geoip to return the users location!

--- example/handler.py
+++ example/handler.py
@@ -1,24 +1,17 @@
 import json
 
+import geoip2.database
 
-def hello(event, context):
-    body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
-    }
 
-    response = {
-        "statusCode": 200,
-        "body": json.dumps(body)
-    }
-
-    return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
+def geoip(event, context):
+    reader = geoip2.database.Reader("/opt/maxminddb/GeoLite2-City.mmdb")
+    response = reader.city(event["requestContext"]["identity"]["sourceIp"])
     return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
+        "body": json.dumps(
+            {
+                "city": response.city.name,
+                "state": response.subdivisions.most_specific.name,
+                "country": response.country.name,
+            }
+        )
     }
-    """
